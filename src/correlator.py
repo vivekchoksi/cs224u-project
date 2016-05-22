b982c6490a4caf98e211bb82183cf6baa51dde58
Vivek Choksi <viv.choksi@gmail.com>
2016-05-21 18:01:36 -0700

    Prune out rare words; compute cohorts without precomputing correlations

--- src/correlator.py
+++ src/correlator.py
@@ -91,7 +91,8 @@ class AmericanBestsellersReader(Reader):
       raise ValueError('Invalid era.')
 
     start_era = self.START_YEAR + era * self.era_size
-    end_era = self.START_YEAR + (era + 1) * self.era_size
+    end_era = min(self.START_YEAR + (era + 1) * self.era_size,
+      self.END_YEAR + 1)
 
     filenames = []
     corpus_dir = os.path.join(DATA_DIR, 'ebooks-unzipped')
@@ -108,8 +109,10 @@ class Correlator():
 
   Usage:
     correlator = Correlator()
-    correlator.preprocess(reader=ExampleReader(), k=20)
-    correlator.get_cohort('hard')
+    correlator.preprocess_counts(reader=Reader())
+    correlator.get_cohort('hard') # Will return after some computation.
+    correlator.preprocess_correlations(k=20)
+    correlator.get_cohort('hard') # Will return immediately.
 
   Attributes:
     counts: a dict mapping from word to numpy array of that word's normalized
@@ -121,47 +124,39 @@ class Correlator():
     self.counts = None
     self.correlations = None
 
-  def preprocess(self, reader=None, k=20, counts_pickle_to_load=None,
-    correlations_pickle_to_load=None, counts_pickle_to_dump=None,
-    correlations_pickle_to_dump=None):
-    """Preprocess the data by counting word occurrences and computing
-    correlations between words, or by loading these values from pickle files.
+  def preprocess_counts(self, reader, pickle_dump_file=None):
+    """Count word frequencies in the corpus.
 
     Args:
-      reader (Optional): an instance of a Reader object.
+      reader: an instance of a Reader object.
+      pickle_dump_file (Optional[str]): name of pickle file to which to
+        write word occurrence count data.
+    """
+    self._count_occurrences(reader)
+    self._prune_counts()
+    if pickle_dump_file is not None:
+      util.pickle_dump(self.counts, pickle_dump_file)
+
+  def load_counts(self, pickle_load_file):
+    self.counts = util.pickle_load(pickle_load_file)
+
+  def preprocess_correlations(self, k=20, pickle_dump_file=None):
+    """Precompute correlations between word pairs.
+
+    Args:
       k (int): the maximum number of top correlated words to keep track of for
         each word.
-      counts_pickle_to_load (Optional[str]): name of pickle file storing word
-        occurrence counts. Either this or `reader` must be specified.
-      correlations_pickle_to_load (Optional[str]): name of pickle file storing
-        correlations between words.
-      counts_pickle_to_dump (Optional[str]): name of pickle file to which to
-        write computed word occurrence counts.
-      correlations_pickle_to_dump (Optional[str]): name of pickle file to which
+      pickle_dump_file (Optional[str]): name of pickle file to which
         to write computed word correlations.
     """
-    # Get word counts, either by reading the corpus or loading a pickle file.
-    if counts_pickle_to_load is None:
-      if reader is None:
-        raise ValueError(
-          'Must either specify a reader or a counts pickle file.')
-      else:
-        self._count_occurrences(reader)
-        if counts_pickle_to_dump is not None:
-          util.pickle_dump(self.counts, counts_pickle_to_dump)
-    else:
-      self.counts = util.pickle_load(counts_pickle)
+    self._correlate_words(k)
+    if pickle_dump_file is not None:
+      util.pickle_dump(self.correlations, pickle_dump_file)
 
-    # Get word correlations, either by reading from word counts or by loading
-    # a pickle file.
-    if correlations_pickle_to_load is None:
-      self._correlate_words(k)
-      if correlations_pickle_to_dump is not None:
-        util.pickle_dump(self.correlations, correlations_pickle_to_dump)
-    else:
-      self.correlations = util.pickle_load(correlations_pickle)
+  def load_correlations(self, pickle_load_file):
+    self.correlations = util.pickle_load(pickle_load_file)
 
-  def get_cohort(self, word):
+  def get_cohort(self, word, k=20):
     """Given a word, return the group of words in its semantic cohort.
 
     Args:
@@ -171,9 +166,15 @@ class Correlator():
       list: a list of tuples of the form
         (word, Pearson correlation coefficient).
     """
-    if self.correlations is None:
-      raise Exception('Must preprocess first by calling preprocess().')
-    return self.correlations[word]
+    if self.counts is None:
+      raise Exception('Counts not yet computed. Must call ' +
+        'preprocess_counts() or load_counts().')
+    elif self.correlations is not None and word in self.correlations:
+      # The cohort is precomputed.
+      return self.correlations[word][:k]
+    else:
+      # Compute the cohort now.
+      return self._correlate_word(word, k)
 
   def _count_occurrences(self, reader):
     """Calculate normalized word frequencies for each word by era as
@@ -182,7 +183,7 @@ class Correlator():
     Args:
       reader: an instance of a Reader object.
     """
-    logging.info('Counting occurences of words...')
+    logging.info('Counting occurrences of words...')
     num_eras = reader.get_num_eras()
 
     counts = defaultdict(lambda: np.zeros(num_eras))
@@ -201,6 +202,16 @@ class Correlator():
 
     self.counts = dict(counts)
 
+  def _prune_counts(self):
+    logging.info('Discarding rare words...')
+    original_length = len(self.counts)
+    for word in self.counts.keys():
+      # Only keep words that were present in every era.
+      if 0 in self.counts[word]:
+        del self.counts[word]
+    logging.info('\tVocabulary size reduced from {} to {}'.format(
+      original_length, len(self.counts)))
+
   def _correlate_words(self, k):
     """Calculate similar word pairs based on their frequency counts as
     self.correlations.
@@ -212,45 +223,55 @@ class Correlator():
         each word.
     """
     if self.counts is None:
-      raise Exception('Must calculate word counts first, by calling count_occurrences().')
+      raise Exception(
+        'Must calculate word counts first, by calling count_occurrences().')
 
     logging.info('Calculating correlations between words...')
-
     correlations = defaultdict(lambda: {})
 
-    print 'Vocabulary size:', len(self.counts)
-    # for word in self.counts:
-    #   print word, self.counts[word]
     counter = 0
-    
+
+    # NOTE(vivekchoksi): Might be faster if this operation were vectorized;
+    # that is, compute Pearson coefficients using matrices instead of a loop.
     for w1 in self.counts:
-      for w2 in self.counts:
-        if w1 is not w2:
-          correlations[w1][w2] = pearsonr(self.counts[w1], self.counts[w2])[0]
+      correlations[w1] = self._correlate_word(w1, k)
 
       counter += 1
       if counter % 10 == 0:
-        logging.info('Correlating...')
-        print 'Finished correlating', counter, 'of', len(self.counts), 'words...'
+        logging.info('\tFinished correlating {} of {} words...'.format(
+          counter, len(self.counts)))
 
-      # To save space, only store top k correlations. That is, for each w1,
-      # only store k w2. The trade-off is that there is redundant computation.
-      correlations[w1] = sorted(correlations[w1].items(),
-        key=operator.itemgetter(1), reverse=True)[:k]
+      if counter == 150:
+        break
 
     self.correlations = dict(correlations)
 
+  def _correlate_word(self, word, k):
+    # Correlations for `word`. E.g. correlations[word2] = Pearson coefficient
+    correlations = {}
+
+    for word2 in self.counts:
+      if word is not word2:
+        correlations[word2] = pearsonr(self.counts[word], self.counts[word2])[0]
+
+    # To save space, only store top k correlations. That is, for each w1,
+    # only store k w2. The trade-off is that there is redundant computation.
+    return sorted(correlations.items(), key=operator.itemgetter(1), reverse=True)[:k]
+
+
 def run_correlator():
-    correlator = Correlator()
-    correlator.preprocess(reader=Reader(), k=20, counts_pickle_to_dump='example_counts.pickle',
-    correlations_pickle_to_dump='example_correlations.pickle')
-    print correlator.get_cohort('great')
-    pdb.set_trace()
+  correlator = Correlator()
+  # correlator.preprocess_counts(AmericanBestsellersReader(), 'ab_counts.pickle')
+  correlator.load_counts('ab_counts.pickle')
+  # correlator.preprocess_correlations(k=20, pickle_dump_file='ab_correlations.pickle')
+  # correlator.load_correlations('ab_correlations.pickle')
+  print correlator.get_cohort('great')
+  pdb.set_trace()
 
 def main():
-    logging.basicConfig(format='[%(name)s %(asctime)s]\t%(msg)s',
-      stream=sys.stderr, level=logging.DEBUG)
-    run_correlator()
+  logging.basicConfig(format='[%(name)s %(asctime)s]\t%(msg)s',
+    stream=sys.stderr, level=logging.DEBUG)
+  run_correlator()
 
 if __name__ == '__main__':
-    main()
+  main()
